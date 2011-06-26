cd7731386b817a1c798d5a8cfa7e9fca73467801
Leif Wickland <leifwickland@gmail.com>
2011-06-26 13:56:09 -0600

    Convenience script for writing HBase tables to disk.

---
+++ bin/hscan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <tableName> [outputFile]"
+	exit
+fi
+if [ $# -lt 2]; then
+	OUT=$1
+else
+	OUT=$2
+fi
+OLDCOLS=$COLUMNS
+stty cols 9999 
+echo "scan '$1'" | hbase shell | sed 's/\s\s\+/ /g' | sed 's/timestamp=\d\+/ts=x/' > $OUT 
+stty cols $OLDCOLS
