eea79318cc50be8664f9862faef6e50a2b2d9ef8
Leif Wickland <leifwickland@gmail.com>
2011-07-01 00:02:11 -0600

    Dumps an HBase table into a format that makes it easy to compare, regardless of timestamp, with another table.

--- bin/hscan.sh
+++ bin/hscan.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-	echo "Usage: $0 <tableName> [outputFile]"
-	exit
+  echo "Usage: $0 <tableName> [outputFile]"
+  exit
 fi
-if [ $# -lt 2]; then
-	OUT=$1
+if [ $# -lt 2 ]; then
+  OUT=$1
 else
-	OUT=$2
+  OUT=$2
 fi
+
+# I guess this doesn't get set when bash executes a shell script...
+COLUMNS=`stty size | cut -d ' ' -f 2`
 OLDCOLS=$COLUMNS
 stty cols 9999 
-echo "scan '$1'" | hbase shell | sed 's/\s\s\+/ /g' | sed 's/timestamp=\d\+/ts=x/' > $OUT 
+echo "scan '$1'" | hbase shell | sed -r -e 's/[ ]{2,}/ /g' -e 's/timestamp=[0-9]+/ts=X/' > $OUT 
 stty cols $OLDCOLS
