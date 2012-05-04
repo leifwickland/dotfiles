a516d3703cfe59590a3e1ab9d86d7516ed00f4d4
Leif Wickland <leifwickland@gmail.com>
2012-05-04 14:04:11 -0600

    Maybe this is a general enough solution to stop annoying me.

--- bin/cvsdiff.rb
+++ bin/cvsdiff.rb
@@ -1,23 +1,20 @@
 #! /nfs/users/ma/lwickland/.ruby-1.6.8/usr/local/bin/ruby
-#puts `pwd`
-#if %r{^.*/trunk[^/]*/?(.*?)$} =~ `pwd`
-if %r{^.*/lwickland/src/[^/]+/?(.*?)$} =~ `pwd`
-  workingDir = $1
-else
-  puts "You're not in a directory that's parented by 'lwickland/src'.  I don't know what to think."
+repo = `cat CVS/Repository`.strip
+if repo == "" 
+  puts " The current directory doesn't have a CVS/Repository file.  I give up."
   exit
 end
-output = `cvs diff 2> /dev/null`
-output.each {|line|
-  if %r{^RCS file: /nfs/src/cvsroot/(?:rnt/)?((?:test|rnw)/.*?),v$} =~ line
-    filename = $1.sub(/\/Attic\//, '/')
-    if (workingDir.length == 0)
-      puts filename
-    elsif filename[0,workingDir.length] === workingDir
-      puts filename[(workingDir.length + 1)..(-1)]
-    else
-      puts "That's odd.  #{filename} doesn't contain the working directory, '#{workingDir}'."
-      exit
-    end
+# I tend to work in CVS repos where the first segment doesn't match the directory it's actually on disk.
+if %r{^[^/]+/(.+)$} =~ repo
+  repo = $1
+end
+diffs = `cvs -q diff 2> /dev/null | grep RCS.file`
+puts "Diffs\n" 
+puts diffs
+diffs.each {|line|
+  if /^.*#{repo}\/(.+),v$/ =~ line
+    puts $1
+  else 
+    puts "I couldn't get #{line} to match #{repo}."
   end
 }
