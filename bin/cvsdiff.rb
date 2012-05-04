8c30349c8c864968ad44d6375396244e05347380
Leif Wickland <leifwickland@gmail.com>
2012-05-04 14:19:09 -0600

    It never works for all cases on the first try.

--- bin/cvsdiff.rb
+++ bin/cvsdiff.rb
@@ -1,4 +1,4 @@
-#! /nfs/users/ma/lwickland/.ruby-1.6.8/usr/local/bin/ruby
+#! /usr/bin/env ruby
 repo = `cat CVS/Repository`.strip
 if repo == "" 
   puts " The current directory doesn't have a CVS/Repository file.  I give up."
@@ -9,9 +9,7 @@ if %r{^[^/]+/(.+)$} =~ repo
   repo = $1
 end
 diffs = `cvs -q diff 2> /dev/null | grep RCS.file`
-puts "Diffs\n" 
-puts diffs
-diffs.each {|line|
+diffs.split("\n").each {|line|
   if /^.*#{repo}\/(.+),v$/ =~ line
     puts $1
   else 
