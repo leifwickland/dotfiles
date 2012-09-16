137e3bb7665bdf85802d3d5a7f7a643be94db635
Leif Wickland <leifwickland@gmail.com>
2012-09-15 22:12:36 -0600

    Print which server the response comes from.

--- bin/captureServerVersions.sh
+++ bin/captureServerVersions.sh
@@ -2,6 +2,8 @@
 
 for i in ca mw gb ; do
   echo -n "$i: "
-  curl "http://vipacs${i}01.rightnowtech.com/version"
-  echo "" 
+  response=$(curl -v "http://vipacs${i}01.rightnowtech.com/version" 2>&1)
+  version="$(echo "$response" | tail -n 1)"
+  server="$(echo "$response" | grep 'RNT-Machine' | sed 's/.*RNT-Machine: //')"
+  echo "$version from server $server" 
 done
