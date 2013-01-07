3186bc338a5fc46ed11d5031f7db18809b441e06
Leif Wickland <leifwickland@gmail.com>
2013-01-07 15:10:05 -0700

    Madison has a different OS than the older build servers.

--- .bashrc.local/madison.pre.sh
+++ .bashrc.local/madison.pre.sh
@@ -1,14 +1,4 @@
-if [ -n "$TERM" -a "$TERM" != "dumb" ]; then 
-    if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then 
-        better_bash=/nfs/project/aarnone/bash/install/bin/bash
-        if [ -x $better_bash ] ; then
-            exec $better_bash
-            if [ "$?" -eq 0 ]; then
-                exit # If everything went well with executing the better bash, then when it's done, exit this one.
-            fi
-        fi
-        echo ""
-        echo "Sadly, you're running a version of bash from many years ago."
-        echo ""
-    fi
-fi
+so buildserver.pre.sh
+
+# The libperl.so that vim depends on is in a different place on madison than the old dev servers.
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/perl5/CORE/
