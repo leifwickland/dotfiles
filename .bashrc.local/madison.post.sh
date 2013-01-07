c62efff90448f75c165b1127d2561939168c125b
Leif Wickland <leifwickland@gmail.com>
2013-01-07 15:22:53 -0700

    Madison has a different OS than the older build servers.

--- .bashrc.local/madison.post.sh
+++ .bashrc.local/madison.post.sh
@@ -1,1 +1,7 @@
-buildserver.post.sh
+# Load the basic buildserver post script.
+source "$( dirname "${BASH_SOURCE[0]}" )/buildserver.post.sh"
+echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+
+# The libperl.so that vim depends on is in a different place on madison than the old dev servers.
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/perl5/CORE/
+echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
