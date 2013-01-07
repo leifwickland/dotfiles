1a74510cd53e52d1efec7ea969128fa4b375f8e6
Leif Wickland <leifwickland@gmail.com>
2013-01-07 15:12:10 -0700

    Madison has a different OS than the older build servers.

--- .bashrc.local/madison.pre.sh
+++ .bashrc.local/madison.pre.sh
@@ -1,4 +1,4 @@
-so buildserver.pre.sh
+. buildserver.pre.sh
 
 # The libperl.so that vim depends on is in a different place on madison than the old dev servers.
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/perl5/CORE/
