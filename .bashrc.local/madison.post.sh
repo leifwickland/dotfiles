e3f58a758ff56da27135bb04c0ad38ff889a17d5
Leif Wickland <leifwickland@gmail.com>
2013-01-24 16:10:27 -0700

    Anthony moved around a tools.

--- .bashrc.local/madison.post.sh
+++ .bashrc.local/madison.post.sh
@@ -2,6 +2,9 @@
 source "$( dirname "${BASH_SOURCE[0]}" )/buildserver.post.sh"
 
 # The vim on the new build servers is new enough. Getting the NFS vim to work was going poorly.
-alias vim='/usr/bin/vim -X'
-alias vi='/usr/bin/vim -X'
-EDITOR='/usr/bin/vim -X'
+alias vi='vim'
+EDITOR='/nfs/project/aarnone/OEL6/vim/current/bin/vim'
+
+pathmunge "/nfs/project/aarnone/OEL6/vim/current/bin/"
+pathmunge "/nfs/project/aarnone/OEL6/ctags/current/bin"
+pathmunge "/nfs/project/aarnone/OEL6/tig/current/bin"
