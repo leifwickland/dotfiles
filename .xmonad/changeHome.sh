e8e862c4aee4a345c807e2d22041d41cdc4ad7fc
Leif Wickland <leifwickland@gmail.com>
2014-01-23 11:34:25 -0700

    Reset caps to escape when switching monitors.
    Coming out of sleep often resets caps, and I often switch monitors right
    after coming out of sleep.

--- .xmonad/changeHome.sh
+++ .xmonad/changeHome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 xrandr --output DP3 --off --output DP1 --off --output DP2 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output eDP1 --off
-xrandr --output DP3 --off --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off
+xrandr --output DP3 --off --output eDP1 --mode 1366x768 --pos 1920x0 --output DP1 --off --output DP2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --rotate normal
 
 killall stalonetray
 # Line by line, the options used by default below mean:
--- .xmonad/changeWork.sh
+++ .xmonad/changeWork.sh
@@ -29,3 +29,5 @@ stalonetray \
   &
 
 nitrogen --restore &
+
+~/.capsIsEscape &
